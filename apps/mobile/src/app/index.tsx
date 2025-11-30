be7d8b8c738ccecf48dd165a11da1a691102213a
matfij <mateusz.michal.fijak@gmail.com>
2025-11-30 18:57:06 +0100

    feat: mobile theme

--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
-import { Button, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import { theme, themeComposable } from "../common/theme";
 
 export default function Index() {
   const { t } = useTranslation();
@@ -9,17 +10,81 @@ export default function Index() {
   const onSignUp = () => {};
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Step Up</Text>
+    <View style={styles.mainWrapper}>
+      <Text style={styles.title}>{t("brand.title")}</Text>
 
-      <Button title={t("auth.signIn")} onPress={onSignIn} />
-      <Button title={t("auth.signUp")} onPress={onSignUp} />
+      <View style={styles.inputWrapper}>
+        <Text style={styles.inputLabel}>{t("auth.email")}</Text>
+        <TextInput
+          style={styles.textInput}
+          cursorColor={theme.colors.primary[700]}
+          selectionColor={theme.colors.primary[700]}
+          placeholderTextColor={theme.colors.dark[300]}
+        />
+      </View>
+
+      <View style={styles.inputWrapper}>
+        <Text style={styles.inputLabel}>{t("auth.authCode")}</Text>
+        <TextInput
+          style={styles.textInput}
+          cursorColor={theme.colors.primary[700]}
+          selectionColor={theme.colors.primary[700]}
+          placeholderTextColor={theme.colors.dark[300]}
+        />
+      </View>
+
+      <Pressable style={styles.button} onPress={onSignIn}>
+        <Text style={styles.buttonText}>{t("auth.signIn")}</Text>
+      </Pressable>
+
+      <Pressable style={styles.button} onPress={onSignUp}>
+        <Text style={styles.buttonText}>{t("auth.signUp")}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: theme.spacing.md,
+    backgroundColor: theme.colors.dark[500],
+  },
+  title: {
+    ...themeComposable.typography.h1,
+    color: theme.colors.primary[600],
+  },
+  inputWrapper: {
+    width: "60%",
+  },
+  inputLabel: {
+    ...themeComposable.typography.bodySmall,
+    color: theme.colors.light[200],
+  },
+  textInput: {
+    ...themeComposable.shadows.glow,
+    fontSize: 24,
+    padding: theme.spacing.xs,
+    color: theme.colors.primary[500],
+    paddingVertical: 0,
+    borderWidth: 2,
+    borderRadius: theme.borderRadius.md,
+    borderColor: theme.colors.primary[600],
+    backgroundColor: theme.colors.dark[300],
+  },
+  button: {
+    ...themeComposable.shadows.lg,
+    width: "60%",
+    alignItems: "center",
+    paddingVertical: theme.spacing.sm,
+    backgroundColor: theme.colors.primary[500],
+    borderRadius: theme.borderRadius.md,
+  },
+  buttonText: {
+    ...themeComposable.typography.bodyLarge,
+    fontWeight: 700,
+    color: theme.colors.dark[500],
+  },
+});
