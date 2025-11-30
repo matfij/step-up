0454b8bf4846edebed0027f815f7201845b3884c
matfij <mateusz.michal.fijak@gmail.com>
2025-11-30 15:21:17 +0100

    feat: mobile app localization

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import "../common/i18n";
 
 export default function RootLayout() {
   return <Stack />;
--- apps/mobile/src/app/index.tsx
+++ apps/mobile/src/app/index.tsx
@@ -1,6 +1,13 @@
-import { Text, View } from "react-native";
+import { useTranslation } from "react-i18next";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+  const { t } = useTranslation();
+
+  const onSignIn = () => {};
+
+  const onSignUp = () => {};
+
   return (
     <View
       style={{
@@ -9,7 +16,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Step Up</Text>
+
+      <Button title={t("auth.signIn")} onPress={onSignIn} />
+      <Button title={t("auth.signUp")} onPress={onSignUp} />
     </View>
   );
 }
