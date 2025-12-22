48b695ae0639cf0b16d91d5e109bf1aa6020d0cb
matfij <mateusz.michal.fijak@gmail.com>
2025-12-22 11:15:41 +0100

    feat: repository storage

---
+++ apps/server/StepUpServer/Activity/ActivityEntity.cs
@@ -0,0 +1,26 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StepUpServer.Activity;
+
+public class Activity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.String)]
+    public required string Id { get; set; }
+    public required string UserId { get; set; }
+
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public required ulong StartTime { get; set; } // Unix timestamp ms
+    public required ulong Duration { get; set; } // ms
+
+    public required ulong Distance { get; set; } // meters
+    public required float AverageSpeed { get; set; } // meters per minute
+    public required float TopSpeed { get; set; } // meters per minute
+
+    public required Coordinate[] route { get; set; }
+}
+
+public record struct Coordinate(double Latitude, double Longitude);
---
+++ apps/server/StepUpServer/Activity/ActivityRepository.cs
@@ -0,0 +1,65 @@
+using MongoDB.Driver;
+using StepUpServer.Common;
+
+namespace StepUpServer.Activity;
+
+public interface IActivityRepository
+{
+    Task<Activity> Create(Activity activity);
+    Task<Activity?> GetById(string id);
+    Task<Activity[]> GetByUserId(string userId, int? skip = null, int? take = null);
+    Task<Activity> Update(Activity activity);
+}
+
+public class ActivityRepository : IActivityRepository
+{
+    private readonly IMongoCollection<Activity> _activities;
+
+    public ActivityRepository(IMongoDatabase database)
+    {
+        _activities = database.GetCollection<Activity>("Activities");
+
+        var userIdIndex = Builders<Activity>.IndexKeys.Ascending(a => a.UserId);
+
+        _activities.Indexes.CreateOneAsync(
+            new CreateIndexModel<Activity>(
+                userIdIndex,
+                new CreateIndexOptions { Background = true }
+            )
+        );
+    }
+
+    public async Task<Activity> Create(Activity activity)
+    {
+        await _activities.InsertOneAsync(activity);
+        return activity;
+    }
+
+    public async Task<Activity?> GetById(string id)
+    {
+        return await _activities.Find(a => a.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<Activity[]> GetByUserId(string userId, int? skip = null, int? take = null)
+    {
+        var query = _activities
+           .Find(a => a.UserId == userId)
+           .SortByDescending(a => a.StartTime)
+           .Skip(skip);
+        if (take > 0)
+        {
+            query = query.Limit(take);
+        }
+        return [.. await query.ToListAsync()];
+    }
+
+    public async Task<Activity> Update(Activity activity)
+    {
+        var result = await _activities.ReplaceOneAsync(u => u.Id == activity.Id, activity);
+        if (result.MatchedCount == 0)
+        {
+            throw new ApiException("errors.activityNotFound");
+        }
+        return activity;
+    }
+}
